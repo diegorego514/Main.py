e11c328bff87a8b6d09cce94abaac61036ade840
diegorego514 <130922543+diegorego514@users.noreply.github.com>
2024-07-27 19:17:47 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+@Bot.message ('(f'Hello, world!')') 
+
+else:
+    print('hello' +inp+ 'how is your day?')
+
+
+**// Bot Commands Goes Here //**
+
+Bot.command.Command = if True = Deletes Vírus 
+
+Bot.command.prefix = Prefix($=True):
+
+Bot.command.Command = if True = set discord updates to automatic download
+
+Bot.command.prefix = Prefix ($True)
+
+Bot.command.Command = if True = set security
+**// Bot.Messages.goes here //**
+Bot.msg.('f('(Hello!)')') 
+
+Bot.msg.(Ban @User1
+         reason: spam)
+Bot.message [virus detected, do you want to delete it? [N/y?] 
+
+if prompted yes: deletes the virus 
+
+if not: set the virus into quarantine. 
+
+Bot.def: del 32 viral infections.
+
+** // More commands will soon be avaliable, remember this bot is still in the closed beta versions. //**
+
+client.run(os.getenv{MTI2NjUzOTY1ODM0NzU0NDcwOA.G_5SHA.lpCVDnnmBNlWLRZujFLg5e0k8b0loByBzxGZMU})
